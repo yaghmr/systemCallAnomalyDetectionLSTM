be7197db322d0ea2a5d1a578abfd1d65ddcdfa00
UnusualCoder <yaghmr@gmail.com>
2019-07-25 11:07:09 +0200

    Clean code (2)

--- train.py
+++ train.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import time
+import h5py
 import numpy as np
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 from keras.models import model_from_json
-
 
 import preprocess
 
@@ -18,17 +18,28 @@ batch_size = 50
 feature_dimension = 341
 
 
+def read_hdf5(path):
+    weights = {}
+    keys = []
+    with h5py.File(path, 'r') as f: # open file
+        f.visit(keys.append) # append all keys to list
+        for key in keys:
+            if ':' in key: # contains data if ':' in key
+                print(f[key].name)
+                weights[f[key].name] = f[key].value
+    return weights
+
+
 def save_model_weight_into_file(model, modelname="model.json", weight="model.h5"):
     model_json = model.to_json()
     with open(modelname, "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
     model.save_weights(weight)
-    print("Saved model to disk in {} and {}".format(modelname,weight))
+    print("Saved model to disk in {} and {}".format(modelname, weight))
 
 
-def load_model_and_wieght_from_file(modelname="model.json", weight="model.h5"):
-
+def load_model_and_weight_from_file(modelname="model.json", weight="model.h5"):
     json_file = open(modelname, 'r')
     loaded_model_json = json_file.read()
     json_file.close()
@@ -67,8 +78,8 @@ def build_model():
 
     start = time.time()
 
-    #model.compile(loss="categorical_crossentropy", optimizer='rmsprop', metrics=['accuracy'])
-    model.compile(loss="mse", optimizer="rmsprop")
+    model.compile(loss="categorical_crossentropy", optimizer='rmsprop', metrics=['accuracy'])
+    #model.compile(loss="mse", optimizer="rmsprop")
 
     print("Compilation Time : ", time.time() - start)
     return model
@@ -111,8 +122,11 @@ def run_network(model=None):
 
     print('Training duration (s) : ', time.time() - global_start_time)
 
+    save_model_weight_into_file(model)
+
     return model, y_test, predicted
 
 
 if __name__ == "__main__":
     run_network()
+    #print(read_hdf5('model.h5'))
